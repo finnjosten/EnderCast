4eb9796b927139a170f7602a94d77f78037887bc
Finn Josten <finn.r.josten@proton.me>
2025-09-25 09:44:16 +0200

    v1.2.0 - final (bstats working)

--- src/main/java/com/example/endercast/EnderCast.java
+++ src/main/java/com/example/endercast/EnderCast.java
@@ -2,6 +2,7 @@ package com.example.endercast;
 
 import java.io.File;
 
+import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -9,18 +10,24 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
+import org.bstats.bukkit.Metrics;
 
+import org.jetbrains.annotations.NotNull;
 import net.kyori.adventure.text.Component;
 
 public class EnderCast extends JavaPlugin implements Listener {
     private FileConfiguration messagesConfig;
+
+    private final int pluginID = 27358; // bstats plugin ID
 
     @Override
     public void onEnable() {
         saveDefaultMessagesConfig();
 
         messagesConfig = YamlConfiguration.loadConfiguration(new File(getDataFolder(), "messages.yml"));
+
+        Metrics metrics = new Metrics(this, pluginID); // Start metrics for bstats
+
 
         setupCommands();
     }
